2cd4647c441e051a090e24d0e2ad22ac0c18de31
Hoang Le <tommyle133@gmail.com>
2026-05-06 08:18:54 +0700

    WIP: Doc

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,18 @@
+≠#include "httplib.h"
 #include <print>
 
+using namespace std;
+using namespace httplib;
+
+void callbackHi(const Request &, Response &res) {
+  res.set_content("Inside somethin'", "text/plain");
+}
+
 int main() {
-  std::println("Something new {:2f}", 0.423423);
-  return 0;
+  Server svr;
+
+  svr.Get("/hi", callbackHi);
+
+  println("Start");
+  svr.listen("0.0.0.0", 8080);
 }
