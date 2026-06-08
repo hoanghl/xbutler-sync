7558a30fc55ac777a05ad17751f2401c508b59f7
Hoang H. Le <hoanghuyle@tinyfish.io>
2026-06-08 22:59:20 +0700

    WIP

--- src/client/client.h
+++ src/client/client.h
@@ -1,7 +1,22 @@
 #pragma once
 
+#include <string>
+#include <vector>
+
+using FileList = std::vector<std::string>;
+using HostName = std::string;
+
 namespace client {
 
-void connect(const char* host, int port);
+struct SyncResponse {
+  FileList upload;
+  FileList download;
+};
+
+FileList scan(const std::string path);
+
+SyncResponse syncFile(const HostName host, FileList files);
+
+void fetch(FileList files);
 
 } // namespace client
